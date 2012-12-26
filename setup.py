43bdadcad33751b2ddbdac332106127a938f3492
Laurens Van Houtven <_@lvh.cc>
2012-12-26 21:52:21 +0100

    Add setuptools-based setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(name='txampext',
+      version='20121226',
+      description="Extensions to Twisted's AMP implementation",
+      url='https://github.com/lvh/txampext',
+
+      author='Laurens Van Houtven',
+      author_email='_@lvh.cc',
+
+      packages=find_packages(),
+
+      install_requires=['twisted'],
+
+      license='ISC',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Twisted",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        ])
+
